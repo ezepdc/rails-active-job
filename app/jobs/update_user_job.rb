6a4c22b110be274bd1ddd7d31bf93b43aa6af851
Ezequiel <epuyosdacosta@gmail.com>
2023-01-23 12:09:58 +0100

    Create update user with background job. Sidekiq and Redis

---
+++ app/jobs/update_user_job.rb
@@ -0,0 +1,10 @@
+class UpdateUserJob < ApplicationJob
+  queue_as :default
+
+  def perform(user_id)
+    user = User.find(user_id)
+    puts "Calling API for #{user.email}"
+    sleep 3
+    puts "OK I'm done now"
+  end
+end
